7f2d5a671a0b05190c8ed0298dab7b393a30b840
James Yost <jyost@localhost.localdomain>
2019-11-24 15:28:50 -0500

    Error handleling

--- pushtotalk.py
+++ pushtotalk.py
@@ -172,7 +172,8 @@ class SampleAssistant(object):
                             break
             
             except:
-                continue
+				logging.info('Error quota exceded shutting down project')
+				os.system("shutdown(0)")
 
 
     @retry(reraise=True, stop=stop_after_attempt(3),
